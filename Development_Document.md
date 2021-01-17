497de2bbb1dcf7172cb7f4a3e7ed4d3a7fd9b5e8
Libro <731096310@qq.com>
2021-01-17 20:41:48 +0800

    inert 00

---
+++ Development_Document.md
@@ -0,0 +1,106 @@
+# development_document_Libro
+
+## 实现目标
+
+#### 图书管理系统
+
+#### 注意点：鲁棒性
+
+## 文件存储
+
+* 文件A
+  * 用来存放书目的具体信息
+  * 生成的索引在其他文件中储存
+
+* 文件B
+  * 存放用户信息
+
+* 文件C
+  * 
+* 文件D
+* 文件raw_log
+  * 日志文件
+
+## 块状链表类*Unrolled_Linked_List* 
+
+#### 拓展功能
+
+内存池
+
+#### 接口
+
+构造函数
+
+* `Unrolled_Linked_List()`
+
+析构函数
+
+* `~Unrolled_Linked_List()`
+
+Add函数
+
+Find函数
+
+Delete函数
+
+## 日志记录类*Watcher*
+
+#### 数据成员
+
+费用记录
+
+#### 接口
+
+构造函数：
+
+* `Watcher(std::string file_name)`
+
+  与输出的文件关联
+
+* `default`
+
+析构函数：
+
+* `~Watcher()`
+
+  关闭输出文件
+
+文件定向函数：
+
+* `void setfile(std::string file_name)`
+
+  设置关联的输出文件，定向前检查是否已经与文件关联，如是，则关闭已经打开的文件后重定向。
+
+输出函数：
+
+* `void print(std::string Message)`
+  * 两份文件，一份美观，一份实用
+
+#### 输出格式
+
+要求：美观
+
+## 命令行接受处理类 *Apollo*
+
+构造函数
+
+析构函数
+
+listen函数
+
+* 接收一行命令并处理，返回一个消息传递类*PaperCup*
+
+
+
+## 消息传递类 *PaperCup*
+
+成员变量
+
+## root用户类 *Markus*
+
+## clerk用户类 *Conner*
+
+## customer用户类 *Kara*
+
+## Base类 *Base*
+
--- Unrolled_Linked_List.hpp
+++ Unrolled_Linked_List.hpp
@@ -4,14 +4,16 @@
 
 #ifndef SRC_UNROLLED_LINKED_LIST_HPP
 #define SRC_UNROLLED_LINKED_LIST_HPP
+
 #include <fstream>
 #include <string>
 #include <cstring>
+
 #define rc(x) reinterpret_cast<char*>(&x)
-#define sfb std::fstream::beg
+#define sfc std::fstream::cur
 
 namespace ULL {
-    const int MAXN=300;
+    const int MAXN = 300;
 
     template<int Key_Len>
     class Unrolled_Linked_List {
@@ -19,80 +21,127 @@ namespace ULL {
 
         int block_num;
         std::fstream file;
-        class block{
+
+        class block {
             friend Unrolled_Linked_List<Key_Len>;
         private:
-            int next,num;
-            class Node{
+            int next;
+
+            class Node {
             public:
-                char key[Key_Len]={0};
+                char key[Key_Len] = {0};
                 int pos;
             } data[MAXN];
 
+            int num;
         public:
-            block():next(0),num(0){}
+            block() : next(0), num(0) {}
         };
-        const int block_size=sizeof(block_num);
-        bool cmp(char a[],char b[]){
-            for (int i=0;i<Key_Len;++i)
-                if (a[i]<b[i]) return 1;
-                else if (a[i]>b[i]) return 0;
+
+        const int block_size = sizeof(block_num);
+
+        bool cmp(char a[], char b[]) {
+            for (int i = 0; i < Key_Len; ++i)
+                if (a[i] < b[i]) return 1;
+                else if (a[i] > b[i]) return 0;
             return 1;
         }
 
     public:
-        Unrolled_Linked_List(std::string file_name):block_num(0){
-            file.open(file_name,std::fstream::binary|std::fstream::out|std::fstream::in);
-            file.write(rc(block_num),sizeof(block_num));
+        Unrolled_Linked_List(std::string file_name) : block_num(0) {
+            file.open(file_name, std::fstream::binary | std::fstream::out | std::fstream::in);
+            file.write(rc(block_num), sizeof(block_num));
         }
-        ~Unrolled_Linked_List(){
+
+        ~Unrolled_Linked_List() {
             file.close();
         }
-        void add(char target[],int& pos){
+
+        void insert(char target[], int &pos) {
             block tmp;
-            int nxt,nxtt,cur,tmp_num;
+            int nxt, nxtt, cur, tmp_num;
             char tmp_key[Key_Len];
-            if (!block_num){
+            //empty
+            if (!block_num) {
                 ++block_num;
-                tmp.data[tmp.num++]={target,pos};
-                file.seekp(sizeof(int),sfb);
-                file.write(rc(tmp),block_size);
+                tmp.data[tmp.num++] = {target, pos};
+                file.seekp(sizeof(int));
+                file.write(rc(tmp), block_size);
                 return;
             }
-            cur=sizeof(int);
-            file.seekg(cur,sfb);
-            file.read(rc(nxt),sizeof(int));
-            while (1){
+            //skip block number
+            cur = sizeof(int);
+            file.seekg(cur);
+            file.read(rc(nxt), sizeof(int));
+            //find the position to insert
+            while (1) {
                 if (!nxt) {
                     break;
                 }
-                file.seekg(nxt,sfb);
-                file.read(rc(nxtt),sizeof(int));
-                file.read(rc(tmp_num),sizeof(int));
-                file.read(tmp_key,Key_Len);
-                if (cmp(tmp_key,target)) {
-                    cur=nxt;
-                    nxt=nxtt;
+                file.seekg(nxt);
+                file.read(rc(nxtt), sizeof(int));
+                file.read(tmp_key, Key_Len);
+                if (cmp(tmp_key, target)) {
+                    cur = nxt;
+                    nxt = nxtt;
                     continue;
                 }
                 break;
             }
-            file.seekg(cur,sfb);
-            file.read(rc(tmp),block_size);
-            if (tmp.num==MAXN){
-
-            }else {
+            //insert
+            file.seekg(cur);
+            file.read(rc(tmp), block_size);
+            //add new block
+            if (tmp.num == MAXN) {
+                block another;
+                //change the pointer
+                another.next = tmp.next;
+                tmp.next = sizeof(int) + block_num * block_size;
+                ++block_num;
+                bool have_insert = false;
+                int i, j;
+                for (j = i = 0; i < MAXN >> 1;) {
+                    if (have_insert||cmp(target, tmp.data[i + (MAXN >> 1)].key)) {
+                        another.data[j++] = tmp.data[i++ + (MAXN >> 1)];
+                    }else {
+                        another.data[j++]={target,pos};
+                        have_insert=true;
+                    }
+                }
+                another.num=j;
+                if (!have_insert) {
+                    tmp.num=(MAXN>>1);
+                    int k=0;
+                    for (;k<tmp.num;++k)
+                        if (cmp(target,tmp.data[k].key)) break;
+                    ++tmp.num;
+                    for (int kk=k+1;kk<tmp.num;++kk) tmp.data[kk]=tmp.data[kk-1];
+                    tmp.data[k]={target,pos};
+                }else {
+                    tmp.num=MAXN>>1;
+                }
+                file.seekp(cur);
+                file.write(tmp);
+                file.seekp(sizeof(int)+(block_num-1)*block_size);
+                file.write(another);
+            }//insert in the block
+            else {
                 int i;
-                for (i=0;i<tmp.num;++i)
-                    if (!cmp(tmp.data[i],target)) break;
+                for (i = 0; i < tmp.num; ++i)
+                    if (cmp(target, tmp.data[i].key)) break;
                 ++tmp.num;
-                for (int j=i+1;j<tmp.num;++j) tmp.data[j]=tmp.data[j-1];
+                for (int j = i + 1; j < tmp.num; ++j) tmp.data[j] = tmp.data[j - 1];
+                tmp.data[i] = {target, pos};
+                file.seekp(cur);
+                file.write(rc(tmp), block_size);
             }
         }
-        int find(char target[]){
+
+        int find(char target[]) {
 
         }
-        void Delete(char target[]){
+
+        void Delete(char target[]) {
 
         }
     };
