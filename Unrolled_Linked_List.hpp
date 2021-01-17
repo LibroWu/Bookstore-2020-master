87602e86b8d26d25c013dc3af34234fe7c6d4fde
Libro <731096310@qq.com>
2021-01-17 23:42:19 +0800

    close in every func

--- Unrolled_Linked_List.hpp
+++ Unrolled_Linked_List.hpp
@@ -30,15 +30,17 @@ namespace ULL {
 
         int block_num, init;
         std::fstream file;
-
+        std::string file_name;
         class block {
             friend Unrolled_Linked_List<Key_Len>;
-            friend std::ostream & operator<<(std::ostream &out,const block& obj){
-                out<<"number of the data="<<obj.num<<"\n";
-                for (int i=0;i<obj.num;++i)
-                    out<<"***"<<obj.data[i].key<<"  "<<obj.data[i].pos<<"***\n";
+
+            friend std::ostream &operator<<(std::ostream &out, const block &obj) {
+                out << "number of the data=" << obj.num << "\n";
+                for (int i = 0; i < obj.num; ++i)
+                    out << "***" << obj.data[i].key << "  " << obj.data[i].pos << "***\n";
                 return out;
             }
+
         private:
             int next;
 
@@ -62,25 +64,26 @@ namespace ULL {
         }
 
     public:
-        Unrolled_Linked_List(std::string file_name) : block_num(0) {
+        Unrolled_Linked_List(std::string file_name) :file_name(file_name), block_num(0) {
             file.open(file_name, std::fstream::binary | std::fstream::out | std::fstream::in);
             file.write(rc(block_num), sizeof(block_num));
             init = sizeof(int);
-        }
-
-        ~Unrolled_Linked_List() {
             file.close();
         }
 
+        ~Unrolled_Linked_List() {
+        }
+
         void insert(char target[Key_Len], int &pos) {
+            file.open(file_name);
             block tmp;
             int nxt, nxtt, cur, tmp_num;
             char tmp_key[Key_Len];
             //empty
             if (!block_num) {
                 ++block_num;
-                strcpy(tmp.data[tmp.num++].key,target);
-                tmp.data[tmp.num++].pos=pos;
+                strcpy(tmp.data[tmp.num++].key, target);
+                tmp.data[tmp.num++].pos = pos;
                 file.seekp(sizeof(int));
                 file.write(rc(tmp), block_size);
                 return;
@@ -123,8 +126,8 @@ namespace ULL {
                     for (int i = 0; i < another.num; ++i) another.data[i] = tmp.data[i + (MAXN >> 1)];
                     tmp.num = 1 + (MAXN >> 1);
                     for (int i = to_insert + 1; i < tmp.num; ++i) tmp.data[i] = tmp.data[i - 1];
-                    strcpy(tmp.data[to_insert].key,target);
-                    tmp.data[to_insert].pos=pos;
+                    strcpy(tmp.data[to_insert].key, target);
+                    tmp.data[to_insert].pos = pos;
                 }
                 else {
                     tmp.num = MAXN >> 1;
@@ -132,15 +135,15 @@ namespace ULL {
                     another.num = (MAXN >> 1) + 1;
                     for (int i = 0; i < another.num; ++i)
                         if (j == to_insert) {
-                            strcpy(another.data[i].key,target);
-                            another.data[i].pos=pos;
+                            strcpy(another.data[i].key, target);
+                            another.data[i].pos = pos;
                         }
                         else another.data[i] = tmp.data[j++];
                 }
                 file.seekp(cur);
-                file.write(rc(tmp),block_size);
+                file.write(rc(tmp), block_size);
                 file.seekp(sizeof(int) + (block_num - 1) * block_size);
-                file.write(rc(another),block_size);
+                file.write(rc(another), block_size);
 
             }//insert in the block
             else {
@@ -149,11 +152,12 @@ namespace ULL {
                     if (cmp(target, tmp.data[i].key)) break;
                 ++tmp.num;
                 for (int j = i + 1; j < tmp.num; ++j) tmp.data[j] = tmp.data[j - 1];
-                strcpy(tmp.data[i].key,target);
-                tmp.data[i].pos=pos;
+                strcpy(tmp.data[i].key, target);
+                tmp.data[i].pos = pos;
                 file.seekp(cur);
                 file.write(rc(tmp), block_size);
             }
+            file.close();
         }
 
         std::vector<int> &find(char target[]) {
@@ -162,6 +166,7 @@ namespace ULL {
                 //todo throw something
                 return *result;
             }
+            file.open(file_name);
             char tmp_key[Key_Len];
             block tmp;
             int cur, nxt, nxtt, pre = 0;
@@ -215,6 +220,7 @@ namespace ULL {
                 }
                 break;
             }
+            file.close();
             return *result;
         }
 
@@ -223,6 +229,7 @@ namespace ULL {
                 //todo throw something
                 return;
             }
+            file.open(file_name);
             char tmp_key[Key_Len];
             block tmp;
             int cur, nxt, nxtt, pre = 0;
@@ -246,20 +253,14 @@ namespace ULL {
                 }
                 break;
             }
-            if (pre) {
-                file.seekg(pre);
-                file.read(rc(tmp), block_size);
-                int i = tmp.num - 1;
-                for (; i >= 0; --i) {
-                    if (tmp.data[i].key != target) break;
-                }
-                tmp.num = i + 1;
-                file.seekp(pre);
-                file.write(rc(tmp), block_size);
-            }
+            bool flag1 = false, flag2 = false;
             while (1) {
                 file.seekg(cur);
                 file.read(rc(tmp), block_size);
+                if (!flag1) {
+                    flag1 = true;
+                    flag2=(tmp.data[0].key==target);
+                }
                 int i, j;
                 for (i = 0; i < tmp.num; ++i)
                     if (tmp.data[i].key == target) break;
@@ -274,7 +275,7 @@ namespace ULL {
                     if (!pre) { init = nxt; }
                     else {
                         file.seekg(pre);
-                        file.write(rc(nxt),sizeof(int));
+                        file.write(rc(nxt), sizeof(int));
                     }
                 }// not empty
                 if (j == tmp.num && nxt) {
@@ -282,6 +283,7 @@ namespace ULL {
                     file.read(rc(nxtt), sizeof(int));
                     file.read(tmp_key, Key_Len);
                     if (tmp_key == target) {
+                        pre = cur;
                         cur = nxt;
                         nxt = nxtt;
                         continue;
@@ -289,23 +291,38 @@ namespace ULL {
                 }
                 break;
             }
+            if (flag2 && pre) {
+                file.seekg(pre);
+                file.read(rc(tmp), block_size);
+                int i = tmp.num - 1;
+                for (; i >= 0; --i) {
+                    if (tmp.data[i].key != target) break;
+                }
+                tmp.num = i + 1;
+                file.seekp(pre);
+                file.write(rc(tmp), block_size);
+            }
+            file.close();
         }
 
 #ifdef debug
 
         void show_the_list() {
             if (!block_num) return;
+            file.open(file_name);
             block tmp;
-            int cur=init,cnt=0;
-            while (1){
+            int cur = init, cnt = 0;
+            while (1) {
                 file.seekg(cur);
-                file.read(rc(tmp),block_size);
-                std::cout<<"block"<<cnt<<":"<<std::endl;
-                std::cout<<tmp<<std::endl;
+                file.read(rc(tmp), block_size);
+                std::cout << "block" << cnt << ":" << std::endl;
+                std::cout << tmp << std::endl;
                 if (!tmp.next) break;
-                cur=tmp.next;
+                cur = tmp.next;
             }
+            file.close();
         }
+
 #endif
     };
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include "Unrolled_Linked_List.hpp"
+using namespace std;
+
+const int key_len=3;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ULL::Unrolled_Linked_List<key_len> l("text.out");
+    vector<int>* result;
+    int n,op,pos;
+    char key[key_len];
+    cin>>n;
+    for (int i=0;i<n;++i){
+        cin>>op;
+        switch (op) {
+            case 0:l.show_the_list();break;
+            case 1:cin>>key>>pos;l.insert(key,pos);break;
+            case 2:
+                cin>>key;
+                result=&l.find(key);
+                for (int j=0;j<result->size();++j) cout<<"---"<<result->operator[](j)<<"---\n";
+                break;
+            default:
+                cin>>key;
+                l.Delete(key);
+        }
+    }
     return 0;
 }
