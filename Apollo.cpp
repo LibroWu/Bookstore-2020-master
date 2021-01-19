9f0d995afd4770e3ce3bd74808f272f4500171ab
Libro <731096310@qq.com>
2021-01-19 19:40:50 +0800

    Apollo: receiving the command

--- Apollo.cpp
+++ Apollo.cpp
@@ -3,3 +3,259 @@
 //
 
 #include "Apollo.h"
+
+
+void GetLine(std::string &s, const char &mark) {
+    char c = getchar();
+    while (c != mark && c != EOF) {
+        s += c;
+        c = getchar();
+    }
+}
+bool Divide(const std::string& input,std::string& first,std::string& second){
+    if (input[0]!='-') return 0;
+    if (input.find('=')==std::string::npos) return 0;
+    unsigned long long pos=input.find('=');
+    first=input.substr(1,pos-1);
+    second=input.substr(pos+1);
+    return 1;
+}
+
+PaperCup &Apollo::listen() {
+    std::string s, token;
+    PaperCup *tmp = new PaperCup;
+    tmp->tokens.clear();
+    GetLine(s, '\n');
+    std::stringstream S_in(s + ' ' + eol);
+    S_in >> token;
+    if (token == "su") {
+        tmp->command_type = 0;
+        S_in >> token;
+        tmp->tokens << token << ' ';
+        S_in >> token;
+        if (token != eol) {
+            tmp->tokens << token << ' ';
+            S_in >> token;
+            if (token != eol) {
+                tmp->command_type = -1;
+                tmp->tokens<<eol;
+                return *tmp;
+            }
+        }
+        tmp->tokens << eol;
+        return *tmp;
+    }
+    else if (token == "logout") {
+        tmp->command_type = 1;
+        S_in >> token;
+        if (token != eol)
+            tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "useradd") {
+        tmp->command_type = 2;
+        for (int i = 0; i < 4; ++i) {
+            S_in >> token;
+            if (token == eol) {
+                tmp->command_type = -1;
+                tmp->tokens<<eol;
+                return *tmp;
+            }
+            tmp->tokens << token << ' ';
+        }
+        S_in >> token;
+        if (token != eol) {
+            tmp->command_type = -1;
+            return *tmp;
+        }
+        tmp->tokens << eol;
+        return *tmp;
+    }
+    else if (token == "register") {
+        tmp->command_type = 3;
+        for (int i = 0; i < 3; ++i) {
+            S_in >> token;
+            if (token == eol) {
+                tmp->command_type = -1;
+                tmp->tokens<<eol;
+                return *tmp;
+            }
+            tmp->tokens << token << ' ';
+        }
+        S_in >> token;
+        if (token != eol) {
+            tmp->command_type = -1;
+            tmp->tokens<<eol;
+            return *tmp;
+        }
+        tmp->tokens << eol;
+        return *tmp;
+    }
+    else if (token == "delete") {
+        tmp->command_type = 4;
+        S_in >> token;
+        if (token == eol) {
+            tmp->command_type = -1;
+            tmp->tokens<<eol;
+            return *tmp;
+        }
+        tmp->tokens << token << ' ';
+        S_in >> token;
+        if (token != eol) {
+            tmp->command_type = -1;
+            tmp->tokens<<eol;
+            return *tmp;
+        }
+        tmp->tokens << eol;
+        return *tmp;
+    }
+    else if (token == "passwd") {
+        tmp->command_type = 5;
+        int cnt = 0;
+        S_in >> token;
+        while (token != eol) {
+            ++cnt;
+            tmp->tokens << token << ' ';
+            S_in >> token;
+        }
+        if (cnt == 2 || cnt == 3) {
+            tmp->tokens << eol;
+            return *tmp;
+        }
+        tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "select") {
+        tmp->command_type = 6;
+        int cnt = 0;
+        S_in >> token;
+        while (token != eol) {
+            ++cnt;
+            tmp->tokens << token << ' ';
+            S_in >> token;
+        }
+        if (cnt == 1) {
+            tmp->tokens << eol;
+            return *tmp;
+        }
+        tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "modify") {
+        tmp->command_type=7;
+        S_in>>token;
+        int cnt=0;
+        std::string first,second;
+        while (token!=eol){
+            if (Divide(token,first,second)){
+                ++cnt;
+                tmp->tokens<<first<<' '<<second<<' ';
+            }else {
+                tmp->command_type=-1;
+                tmp->tokens<<eol;
+                return *tmp;
+            }
+            S_in>>token;
+        }
+        if (cnt>5) tmp->command_type=-1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "import") {
+        tmp->command_type = 8;
+        int cnt = 0;
+        S_in >> token;
+        while (token != eol) {
+            ++cnt;
+            tmp->tokens << token << ' ';
+            S_in >> token;
+        }
+        if (cnt == 2) {
+            tmp->tokens << eol;
+            return *tmp;
+        }
+        tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+        //show and show finance
+    else if (token == "show") {
+        S_in >> token;
+        if (token == eol) {
+            tmp->tokens << eol;
+            tmp->command_type = 9;
+            tmp->tokens<<eol;
+            return *tmp;
+        }
+        if (token == "finance") {
+            tmp->command_type = 10;
+            S_in >> token;
+            if (token == eol) {
+                tmp->tokens<<eol;
+                return *tmp;
+            }
+            tmp->tokens << token << ' ' << eol;
+            S_in >> token;
+            if (token != eol) tmp->command_type = -1;
+            return *tmp;
+        }
+        std::string first,second;
+        if (Divide(token,first,second)){
+            tmp->command_type=9;
+            tmp->tokens<<first<<' '<<second<<' ';
+        }else {
+            tmp->command_type=-1;
+            tmp->tokens<<eol;
+            return *tmp;
+        }
+        S_in>>token;
+        if (token!=eol) tmp->command_type=-1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "buy") {
+        tmp->command_type = 11;
+        int cnt = 0;
+        S_in >> token;
+        while (token != eol) {
+            ++cnt;
+            tmp->tokens << token << ' ';
+            S_in >> token;
+        }
+        if (cnt == 2) {
+            tmp->tokens << eol;
+            return *tmp;
+        }
+        tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+        //report finance and report employee and report myself
+    else if (token == "report") {
+        S_in >> token;
+        if (token == "finance") tmp->command_type = 12;
+        else if (token == "employee") tmp->command_type = 13;
+        else if (token == "myself") tmp->command_type = 15;
+        else tmp->command_type = -1;
+        S_in >> token;
+        if (token != eol) tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else if (token == "log") {
+        S_in >> token;
+        tmp->command_type = 14;
+        if (token != eol)
+            tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+    else {
+        tmp->command_type = -1;
+        tmp->tokens<<eol;
+        return *tmp;
+    }
+}
--- Apollo.h
+++ Apollo.h
@@ -4,14 +4,44 @@
 
 #ifndef SRC_APOLLO_H
 #define SRC_APOLLO_H
+
 #include <string>
-class PaperCup{
-    int argc;
+#include <sstream>
+#include <iostream>
+
+const std::string eol = "#@$%";
+
+void GetLine(std::string &s, const char &mark);
+bool Divide(const std::string& input,std::string& first,std::string& second);
+
+class PaperCup {
+public:
+    //command_type
+    //0 for su, tokens has one or two tokens (user_id and passwd(optional))
+    //1 for logout, tokens is empty
+    //2 for useradd, tokens has four tokens (user_id,passwd,level and name)
+    //3 for register, tokens has three tokens (user_id,passwd and name)
+    //4 for delete, tokens has one token (user_id)
+    //5 for passwd, tokens has two or three tokens (user_id,new_pw,old_pw(optional))
+    //6 for select, tokens has one token ISBN
+    //7 for modify, tokens has even tokens. eg: "ISBN" will be followed by [ISBN]
+    //8 for import, tokens has two tokens. (quantity and cost_price)
+    //9 for show, tokens has zero or two tokens, the same as modify.
+    //10 for show finance, tokens has zero or one token (time)
+    //11 for buy, tokens has two tokens (ISBN and quantity)
+    //12 for report finance
+    //13 for report employee
+    //14 for log
+    //15 for report myself
+    //-1 for invalid command
+    //besides,each tokens will end up with an eol
+    int command_type;
+    std::stringstream tokens;
 };
 
 class Apollo {
 public:
-    PaperCup& listen();
+    PaperCup &listen();
 };
 
 
